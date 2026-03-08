43b6d832b22c52599b593ce242afc5e400c6fd78
pop <apak@andrew.cmu.edu>
2026-03-08 15:15:12 -0400

    chore: ignore target/ build artifacts

--- target/debug/deps/linux_rust_bindings-de2b1ab6a2fc45b2.d
+++
@@ -1,5 +0,0 @@
-/home/pop/linux-c2rust-pipeline/target/debug/deps/linux_rust_bindings-de2b1ab6a2fc45b2.d: crates/linux_rust_bindings/src/lib.rs
-
-/home/pop/linux-c2rust-pipeline/target/debug/deps/liblinux_rust_bindings-de2b1ab6a2fc45b2.rmeta: crates/linux_rust_bindings/src/lib.rs
-
-crates/linux_rust_bindings/src/lib.rs:
--- target/debug/deps/linux_rust_generated-18be1a0eed75fd24.d
+++
@@ -1,6 +0,0 @@
-/home/pop/linux-c2rust-pipeline/target/debug/deps/linux_rust_generated-18be1a0eed75fd24.d: crates/linux_rust_generated/src/lib.rs crates/linux_rust_generated/src/nintendo_otp.rs
-
-/home/pop/linux-c2rust-pipeline/target/debug/deps/liblinux_rust_generated-18be1a0eed75fd24.rmeta: crates/linux_rust_generated/src/lib.rs crates/linux_rust_generated/src/nintendo_otp.rs
-
-crates/linux_rust_generated/src/lib.rs:
-crates/linux_rust_generated/src/nintendo_otp.rs:
